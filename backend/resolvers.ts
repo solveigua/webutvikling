a6785d4d48b3b371935b6834b4cbae8ec99152f2
Solveig Aune <solveig.aune@live.no>
2021-11-17 22:43:35 +0100

    #1 added resolver and schema

--- backend/resolvers.ts
+++ backend/resolvers.ts
@@ -2,9 +2,11 @@
  * Exports all resolvers
  */
 
-import {Movie} from './models/movie'
-import {Character } from './models/character'
-import mongoose from 'mongoose'
+import { Movie } from "./models/movie";
+import { Character } from "./models/character";
+import mongoose from "mongoose";
+
+//TODO: remove unused interfaces
 
 /*interface IMovie {
     id: String;
@@ -29,61 +31,76 @@ interface ratingInput {
 }*/
 
 export const resolvers = {
-    Query: {
-        hello: () => {
-            return "hello world";
-        },
-        getAllMovies: async () => {
-            const movies = await Movie.find();
-            console.log(movies);
-            return movies;
-        },
-
-        getAllCharacters: async () => {
-            const characters = await Character.find();
-            return characters;
-        },
-        getMovie: async (_: any, args: any) => {
-            console.log(args.input.id);
-            try {
-                const movie = await Movie.findById(args.input.id);
-                return movie;
-             }
-             catch (err) {
-                 throw err;
-             }
-        },
-
-        getCharacter: async (_: any, args: any) => {
-            console.log(args.input.id);
-            try {
-                const character = await Character.findById(args.input.id);
-                return character;
-             }
-             catch (err) {
-                 throw err;
-             }
-        }
-
+  Query: {
+    hello: () => {
+      return "hello world";
     },
-    Mutation: {
-        setRating: async (_: any, args: any) => {
-            console.log(args.input);
-            try {
-                const movie = await Movie.findById(args.input.movieId);
-                console.log(JSON.parse(JSON.stringify(movie)));
-                if (movie) {
-                    await Movie.updateOne( movie ,{ $set: { rating: args.input.rating }});
-                    return movie;
-                }
-                else{
-                    throw new Error('Movie does not exist in database. ');
-                }
-            }
-            catch (err) {
-                throw err;
-            }
-        }
-    }
+    getAllMovies: async () => {
+      const movies = await Movie.find();
+      console.log(movies);
+      return movies;
+    },
 
+    getAllCharacters: async () => {
+      const characters = await Character.find();
+      return characters;
+    },
+    getMovie: async (_: any, args: any) => {
+      console.log(args.input.id);
+      try {
+        const movie = await Movie.findById(args.input.id);
+        return movie;
+      } catch (err) {
+        throw err;
+      }
+    },
+
+    getCharacter: async (_: any, args: any) => {
+      console.log(args.input.id);
+      try {
+        const character = await Character.findById(args.input.id);
+        return character;
+      } catch (err) {
+        throw err;
+      }
+    },
+
+    lazyLoading: async (_: any, args: any) => {
+      try {
+        const allMovies = await Movie.find();
+        const start = false;
+        const endResult = [];
+
+        for (let i = args.input.start; i <= allMovies.length; i++) {
+          if (i <= args.input.limit) {
+            endResult.push(allMovies[i]);
+          }
+          if (args.input.limit && endResult.length === args.input.limit) {
+            break;
+          }
+          return endResult;
+        }
+      } catch (err) {
+        throw err;
+      }
+    },
+  },
+
+  Mutation: {
+    setRating: async (_: any, args: any) => {
+      console.log(args.input);
+      try {
+        const movie = await Movie.findById(args.input.movieId);
+        console.log(JSON.parse(JSON.stringify(movie)));
+        if (movie) {
+          await Movie.updateOne(movie, { $set: { rating: args.input.rating } });
+          return movie;
+        } else {
+          throw new Error("Movie does not exist in database. ");
+        }
+      } catch (err) {
+        throw err;
+      }
+    },
+  },
 };
