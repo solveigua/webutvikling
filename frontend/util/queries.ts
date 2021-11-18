19e1a8de232a7b4c9b7fe72890bcea43bd019b20
Solveig Aune <solveig.aune@live.no>
2021-11-18 12:30:11 +0100

    #7 added query

--- frontend/util/queries.ts
+++ frontend/util/queries.ts
@@ -2,57 +2,75 @@ import gql from "graphql-tag";
 
 //Queries:
 const GET_ALL_MOVIES = gql`
-    query{
-        getAllMovies {
-            _id
-            title
-            seqNr
-            releaseYear
-            rating
-          }
+  query {
+    getAllMovies {
+      _id
+      title
+      seqNr
+      releaseYear
+      rating
     }
-`
+  }
+`;
 
 const GET_MOVIE = gql`
-    query($id: ID!){
-        getMovie(input: {id: $id}) {
-            title
-            rating
-            seqNr
-            releaseYear
-          }
+  query($id: ID!) {
+    getMovie(input: { id: $id }) {
+      title
+      rating
+      seqNr
+      releaseYear
     }
-`
+  }
+`;
 
 const GET_ALL_CHARACTERS = gql`
-    query{
-        getAllCharacters {
-            _id
-            name
-            actor
-            appearencesInMovies
-            movies
-          }
+  query {
+    getAllCharacters {
+      _id
+      name
+      actor
+      appearencesInMovies
+      movies
     }
-`
+  }
+`;
 
 const GET_CHARACTER = gql`
-    query($id: ID!){
-        getCharacter (input: {id: $id}){
-            name
-            actor
-            movies
-          }
+  query($id: ID!) {
+    getCharacter(input: { id: $id }) {
+      name
+      actor
+      movies
     }
-`
+  }
+`;
+
+const LAZY_LOADING = gql`
+  query($limit: Int, $start: Int) {
+    lazyLoading(input: { limit: $limit, start: $start }) {
+      title
+      rating
+      seqNr
+      releaseYear
+    }
+  }
+`;
 
 //Mutation:
 const SET_RATING = gql`
-    mutation ($id: ID, $rating: Int!){
-        setRating(input: {movieId: $id, rating: $rating}){
-            title
-            rating
-          }
+  mutation($id: ID, $rating: Int!) {
+    setRating(input: { movieId: $id, rating: $rating }) {
+      title
+      rating
     }
-`
-export {GET_ALL_MOVIES, GET_MOVIE, GET_ALL_CHARACTERS, GET_CHARACTER, SET_RATING }
+  }
+`;
+export {
+  GET_ALL_MOVIES,
+  GET_MOVIE,
+  GET_ALL_CHARACTERS,
+  GET_CHARACTER,
+  SET_RATING,
+  LAZY_LOADING,
+};
